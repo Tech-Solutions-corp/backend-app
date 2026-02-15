c84854a890f72c6b119206e90fa20724327ffa47
ArturMatukiwa <artur.matukiwa@sptech.school>
2026-02-15 11:07:37 -0300

    config inicial

---
+++ src/main/java/org/tech_solutions/application/Startup.java
@@ -0,0 +1,13 @@
+package org.tech_solutions.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
